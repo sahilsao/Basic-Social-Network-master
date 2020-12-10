bbae8cfaba1bde86d8ea9b2ef9ad4e00b7b40ba1
Sahil Sao <51815576+sahilsao@users.noreply.github.com>
2020-12-10 18:37:11 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -123,6 +123,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'network/static/network')
 ]
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 
