cc8f939a42dd2e58ef53ba7b26eff6572543151e
Sahil Sao <51815576+sahilsao@users.noreply.github.com>
2020-12-10 18:27:21 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -46,7 +46,6 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
