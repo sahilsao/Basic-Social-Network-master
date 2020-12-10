8b9ed62f5cea19ed99eed36c139add6170b5a7b9
Sahil Sao <51815576+sahilsao@users.noreply.github.com>
2020-12-10 18:40:30 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'project4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'network/templates/network')],
+        'DIRS': [os.path.join(BASE_DIR,'/templates/network')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'network/static/network')
+    os.path.join(BASE_DIR, '/static/network')
 ]
 
 
