8d1a3d51d9bf596d0806623191cf6ba4eb759eb0
Sahil Sao <51815576+sahilsao@users.noreply.github.com>
2020-12-10 18:00:26 +0530

    Update

--- project4/settings.py
+++ project4/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'project4.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,''],
+        'DIRS': [os.path.join(BASE_DIR,'')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
