933cbbb09936f52207dadca06aff08621f32285a
Sahil Sao <51815576+sahilsao@users.noreply.github.com>
2020-12-10 13:51:17 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -122,6 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.CompressedStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
