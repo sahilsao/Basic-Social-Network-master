e5e6ac0d50627dd6fae9bb8b4ae6ba93a7df5deb
Sahil Sao <sahilfaizal@gmail.com>
2020-12-10 13:19:54 +0530

    final

--- project4/settings.py
+++ project4/settings.py
@@ -121,4 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedStaticFilesStorage'
