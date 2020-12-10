e06722dc209f65588685aa39872454186ee72194
Sahil Sao <51815576+sahilsao@users.noreply.github.com>
2020-12-10 18:52:36 +0530

    Update settings.py

--- project4/settings.py
+++ project4/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['socialjdp.herokuapp.com','127.0.0.1']
 
 INSTALLED_APPS = [
     'network',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,6 +46,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -133,3 +135,4 @@ STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'build/static')
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+CORS_ORIGIN_ALLOW_ALL = True
